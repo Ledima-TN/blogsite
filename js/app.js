d798a2cc54a8d44860c9b062f8de529641b47ba8
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2020-04-26 02:23:27 +0200

    new website

--- js/app.js
+++ js/app.js
@@ -1,18 +1,51 @@
-	  $(document).ready(function () {
-  $('.main-header').lettering();
-  $('.split-text').lettering();
-  $('a.toscroll').on('click', function (e) {
-    var href = $(this).attr('href');
-    e.preventDefault();
-    console.log(href); 
-
-    
-    var hash = href.substring(href.indexOf('#') + 1);
-    $('html, body').animate({
+$(document).ready(function () {
+  var href = '#about'
+  var hash = href.substring(href.indexOf('#') + 1)
+  $('html, body').animate(
+    {
       scrollTop: $('#' + hash).offset().top
-    }, 500);
-    return false;
+    },
+    200
+  )
+
+  $('.main-header').lettering()
+  $('.split-text').lettering()
+  $('a.toscroll').on('click', function (e) {
+    var href = $(this).attr('href')
+    e.preventDefault()
+    console.log(href)
+
+    var hash = href.substring(href.indexOf('#') + 1)
+    $('html, body').animate(
+      {
+        scrollTop: $('#' + hash).offset().top
+      },
+      500
+    )
+    return false
   })
 })
 
+$(function () {
+  $('.navbar-toggle, nav, .main-title').click(function () {
+    $('.navbar-toggle').toggleClass('navbar-on')
+    $('.main-title').toggleClass('navbar-on')
+    $('nav').fadeToggle()
+    $('nav').removeClass('nav-hide')
 
+    var href = $(this).attr('href')
+    if (href) {
+      e.preventDefault()
+      console.log(href)
+
+      var hash = href.substring(href.indexOf('#') + 1)
+      $('html, body').animate(
+        {
+          scrollTop: $('#' + hash).offset().top
+        },
+        500
+      )
+      return false
+    }
+  })
+})
